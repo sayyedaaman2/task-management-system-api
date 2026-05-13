39a9cf77a43570fda047bfb8aed74fc2a7aa60eb
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-05-13 17:28:12 +0530

    Implemented the task model schema

---
+++ src/model/task.model.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+import { TaskStatus,TaskPriority } from "@/utils/constant.js";
+
+export interface ITask {
+  title: string;
+  description: string;
+  status: string;
+  dueDate: Date;
+  assignedTo: mongoose.Types.ObjectId;
+}
+
+export interface ITaskDocument extends ITask, mongoose.Document {}
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: TaskStatus.values,
+      default: TaskStatus.PENDING,
+    },
+    priority: {
+      type  : String,
+      enum  : TaskPriority.values,
+      default: TaskPriority.MEDIUM,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.models.tasks || mongoose.model<ITask>("Task", taskSchema);
+
+export default Task;
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,15 +1,26 @@
-
-
 export const UserTypes = {
-    ADMIN: 'admin',
-    USER: 'user',
-    values: ['admin', 'user']
+  ADMIN: "admin",
+  USER: "user",
+  values: ["admin", "user"],
 } as const;
 
 export const UserStatus = {
-    ACTIVE: 'active',
-    INACTIVE: 'inactive',
-    values: ['active', 'inactive']
+  ACTIVE: "active",
+  INACTIVE: "inactive",
+  values: ["active", "inactive"],
+} as const;
+
+export const TaskStatus = {
+  PENDING: "pending",
+  IN_PROGRESS: "in progress",
+  COMPLETED: "completed",
+  values: ["pending", "in progress", "completed"],
 } as const;
 
 
+export const TaskPriority = {
+  LOW: "Low",
+  MEDIUM: "Medium",
+  HIGH: "High",
+  values: ["Low", "Medium", "High"],
+} as const;
