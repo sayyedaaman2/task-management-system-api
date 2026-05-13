a2458fd64c7a7a97e0d5860b040684181d492c4d
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-05-13 15:12:54 +0530

    User Model Schema Created

---
+++ src/model/user.model.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+import { UserTypes,UserStatus } from "@/utils/constant.js";
+
+export interface IUser {
+    name : string;
+    email : string;
+    password : string;
+    userType : typeof UserTypes.values[number];
+    userStatus : typeof UserStatus.values[number];
+}
+
+
+
+const userSchema = new mongoose.Schema<IUser>({
+    name : {
+        type : String,
+        required : true,
+        max : 50,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    userType : {
+        type : String,
+        enum : UserTypes.values,
+        default : UserTypes.USER,
+    },
+    userStatus : {
+        type : String,
+        enum : UserStatus.values,
+        default : UserStatus.ACTIVE,
+    }
+    
+}, {timestamps : true});
+
+const User = mongoose.models.user || mongoose.model<IUser>('User', userSchema);
+
+export default User;
---
+++ src/utils/constant.ts
@@ -0,0 +1,15 @@
+
+
+export const UserTypes = {
+    ADMIN: 'admin',
+    USER: 'user',
+    values: ['admin', 'user']
+} as const;
+
+export const UserStatus = {
+    ACTIVE: 'active',
+    INACTIVE: 'inactive',
+    values: ['active', 'inactive']
+} as const;
+
+
