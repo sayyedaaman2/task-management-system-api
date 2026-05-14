644edabbb0355320ed2036f391c645bcb7555599
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-05-14 18:08:51 +0530

    fix: task.controller User Authentication issue resolved

--- src/controller/task.controller.ts
+++ src/controller/task.controller.ts
@@ -1,10 +1,21 @@
 import type { Request, Response, NextFunction } from "express";
 
+import type { ITask } from "@/model/task.model.js";
 import taskService from "@/service/task.service.js";
 
 export const createTask = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const task = await taskService.createTask(req.body);
+    const userId = req.user?.userId;
+    const { title, description, dueDate, status }: ITask = req.body;
+
+    const payload = {
+      title,
+      description,
+      dueDate,
+      status,
+      assignedTo: userId,
+    };
+    const task = await taskService.createTask(payload);
     res.status(201).json(task);
   } catch (error) {
     next(error);
@@ -13,8 +24,8 @@ export const createTask = async (req: Request, res: Response, next: NextFunction
 
 export const getAllTasks = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const userId = req.user.UserId
-    const tasks = await taskService.getAllTasks(userId,req.query);
+    const userId = req.user.userId;
+    const tasks = await taskService.getAllTasks(userId, req.query);
     res.status(200).json(tasks);
   } catch (error) {
     next(error);
@@ -23,9 +34,9 @@ export const getAllTasks = async (req: Request, res: Response, next: NextFunctio
 
 export const getTaskById = async (req: Request, res: Response, next: NextFunction) => {
   try {
-        const userId = req.user.UserId
+    const userId = req.user.userId;
 
-    const task = await taskService.getTaskById(userId,req.params.id);
+    const task = await taskService.getTaskById(req.params.id, userId);
     if (!task) {
       return res.status(404).json({ message: "Task not found" });
     }
@@ -37,8 +48,8 @@ export const getTaskById = async (req: Request, res: Response, next: NextFunctio
 
 export const updateTask = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const userId = req.user.UserId
-    const task = await taskService.updateTask(req.params.id,userId, req.body);
+    const userId = req.user.userId;
+    const task = await taskService.updateTask(req.params.id, userId, req.body);
     if (!task) {
       return res.status(404).json({ message: "Task not found" });
     }
@@ -50,8 +61,8 @@ export const updateTask = async (req: Request, res: Response, next: NextFunction
 
 export const deleteTask = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const userId = req.user.UserId
-    const task = await taskService.deleteTask(userId,req.params.id);
+    const userId = req.user.userId;
+    const task = await taskService.deleteTask(req.params.id, userId);
     if (!task) {
       return res.status(404).json({ message: "Task not found" });
     }
